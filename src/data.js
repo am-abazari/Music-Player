ffa13620197badef33f7ef3a72b55bc63ba9791f
Amirhossein Abazari <amirhossein.abazari@yahoo.com>
2021-11-11 03:23:03 +0330

    Created Project Music Player

---
+++ src/data.js
@@ -0,0 +1,129 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+	return [
+		{
+			name: "Yakuza",
+			cover:
+				"https://top1musics.ir/wp-content/uploads/2021/09/Sepehr%20Khalse%20-%20Yakuza-360x360.jpg",
+			artist: "Sepehr Khalse , Tossmoney",
+			audio: "https://dl.top1musics.ir//Sepehr%20Khalse%20-%20Yakuza.mp3",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: true,
+		},
+		{
+			name: "Ghahghara",
+			cover:
+				"https://saz-music.net/wp-content/uploads/2020/03/Bahram-nouraei-Gozaar.jpg",
+			artist: " Bahram ",
+			audio: "http://sv.ghadim-music.ir/archive/b/bahram/Bahram%20-%20Gozaar/Bahram%20-%20Gozaar%20128/1.%20Ghahghara%20[Prod.%20by%20Ashkan%20Mousavi]%20128.mp3",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "2JA",
+			cover:
+				"https://dozmusic.ir/wp-content/uploads/2021/05/Ho3ein-2Ja-480x480.jpg",
+			artist: "Ho3ein",
+			audio: "https://dl.dozmusic.ir/file/1400/03/07/Ho3ein%20-%202Ja.mp3",
+			color: ["#CD607D", "#c94043"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "LaL",
+			cover:
+				"https://behmusic.com/wp-content/webp-express/webp-images/uploads/2020/06/Yas-Lal-496x496.jpg.webp",
+			artist: "Yas",
+			audio: "https://dl2.behmusic.com/music/1399/03/Irani/Yas%20-%20Lal%5B128%5D.mp3",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Sefareshi",
+			cover:
+				"https://www.topseda.ir/wp-content/uploads/2019/02/Yas-Sefareshi.jpg",
+			artist: "Yas",
+			audio: "http://www.s4.topseda.ir/nevisande/hani2/music/98/06/18/Yas%20-%20%27Sefareshi%27%20MP3%20%5B128%5D.mp3",
+			color: ["#CD607D", "#c94043"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "To Bodi",
+			cover:
+				"https://www.topseda.ir/wp-content/uploads/2021/11/ParsaLip%20-%20To%20Boodi.jpg",
+			artist: "Parsalip",
+			audio: "http://s4.topseda.ir/nevisande/neda//1400/08/12/ParsaLip/ParsaLip%20-%20To%20Boodi%20128.mp3",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Viron Kon",
+			cover:
+				"https://www.topseda.ir/wp-content/uploads/2021/08/Erfan%20-%20Viroon%20Kon.jpg",
+			artist: "Erfan",
+			audio: "http://s4.topseda.ir/nevisande/neda//1400/06/09/Erfan/Erfan%20-%20Viroon%20Kon%20128.mp3",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Bego Barikala",
+			cover:
+				"https://www.topseda.ir/wp-content/uploads/2021/09/EpiCure%20-%20Begoo%20Barikalla.jpg",
+			artist: "Epicure",
+			audio: "http://s4.topseda.ir/nevisande/neda//1400/07/01/EpiCure/EpiCure%20-%20Begoo%20Barikalla%20128.mp3",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Garage",
+			cover:
+				"https://www.topseda.ir/wp-content/uploads/2021/10/Hamed%20Fard%20-%20Garage.jpg",
+			artist: "Hamed Fard",
+			audio: "http://s4.topseda.ir/nevisande/neda//1400/07/01/EpiCure/EpiCure%20-%20Begoo%20Barikalla%20128.mp3",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Ashk",
+			cover:
+				"https://www.topseda.ir/wp-content/uploads/2021/10/Behzad%20Leito%20-%20Ashk.jpg",
+			artist: "Behzad Leito",
+			audio: "http://s4.topseda.ir/nevisande/neda//1400/07/20/Behzad%20Leito/Behzad%20Leito%20-%20Ashk%20128.mp3",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Mullahs",
+			cover:
+				"https://www.topseda.ir/wp-content/uploads/2021/01/afta_hill-erfan_mullahs.jpg",
+			artist: "Erfan",
+			audio: "http://s4.topseda.ir/99/10/28/Afta%20Hill%20Mullahs%20%28Ft%20Erfan%29%20%28128%29.mp3",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Ashk",
+			cover:
+				"https://www.topseda.ir/wp-content/uploads/2021/10/Behzad%20Leito%20-%20Ashk.jpg",
+			artist: "Behzad Leito",
+			audio: "http://s4.topseda.ir/nevisande/neda//1400/07/20/Behzad%20Leito/Behzad%20Leito%20-%20Ashk%20128.mp3",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		
+		
+	];
+}
+
+export default chillHop;
